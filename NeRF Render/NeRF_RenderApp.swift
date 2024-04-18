75e9143f7b579d7ad682cfd16764a800cf54a418
EL549 <el549@qq.com>
2024-04-18 19:12:30 +0800

    Initial Commit

---
+++ NeRF Render/NeRF_RenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeRF_RenderApp.swift
+//  NeRF Render
+//
+//  Created by LIU HengYu on 2024/4/18.
+//
+
+import SwiftUI
+
+@main
+struct NeRF_RenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
